c3adc08f922badecb8c340c081de78a5327349b3
charlescript <rocha_charles@outlook.com>
2026-01-03 04:09:42 -0300

    Javacode comentários personalizados com /** */

--- maratona-java/src/main/java/academy/devdojo/maratonajava/introducao/OlaDevDojo.java
+++ maratona-java/src/main/java/academy/devdojo/maratonajava/introducao/OlaDevDojo.java
@@ -1,6 +1,14 @@
 package academy.devdojo.maratonajava.introducao;
 
+/**
+ *  Comentário estilizado no inteliJ <b>javadoc<b/>
+ */
 public class OlaDevDojo {
+    // Isso é um comentário de uma linha
+    /*
+       Comentáro de
+       múltiplas linhas
+    */
     public static void main(String[] args) {
         System.out.println("KA-ME-HA-ME-HA");
     }
