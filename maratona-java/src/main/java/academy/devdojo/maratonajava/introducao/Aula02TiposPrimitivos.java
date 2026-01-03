0fe24f263d0278f546b31d7b3eb60e5f3c3af320
charlescript <rocha_charles@outlook.com>
2026-01-03 05:03:37 -0300

    Trabalhando com tipos primitivos unicode e tabela ascii aula 11/286

--- maratona-java/src/main/java/academy/devdojo/maratonajava/introducao/Aula02TiposPrimitivos.java
+++ maratona-java/src/main/java/academy/devdojo/maratonajava/introducao/Aula02TiposPrimitivos.java
@@ -4,7 +4,20 @@ public class Aula02TiposPrimitivos {
     public static void main(String[] args) {
         // int, double, float, char, byte, short, long, boolean
         int age = 10;
+        long numeroGrande = 100000000;
+        double salarioDouble = 2500;
+        float salarioFloat = 2500;
+        byte idadeByte = -128;
+        short idadeShort = 32767;
+        boolean verdadeiro = true;
+        boolean falso = false;
+        char caractere = '\u0041'; /**
+         * Traduz o decimal para tabela ASCII
+         */
+
 
         System.out.println("Idade: " + age + " anos.");
+        System.out.println(falso);
+        System.out.println("CHAR: " + caractere);
     }
 }
