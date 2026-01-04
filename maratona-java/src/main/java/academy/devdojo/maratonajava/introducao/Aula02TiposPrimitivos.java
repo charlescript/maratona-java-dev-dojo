facb9e97e4e9f1fca03a591ac51b718ccd083b9d
charlescript <rocha_charles@outlook.com>
2026-01-04 15:36:54 -0300

    13/286 Tipos primitivos usando Casting

--- maratona-java/src/main/java/academy/devdojo/maratonajava/introducao/Aula02TiposPrimitivos.java
+++ maratona-java/src/main/java/academy/devdojo/maratonajava/introducao/Aula02TiposPrimitivos.java
@@ -3,12 +3,12 @@ package academy.devdojo.maratonajava.introducao;
 public class Aula02TiposPrimitivos {
     public static void main(String[] args) {
         // int, double, float, char, byte, short, long, boolean
-        int age = 10;
-        long numeroGrande = 100000000;
-        double salarioDouble = 2500;
-        float salarioFloat = 2500;
-        byte idadeByte = -128;
-        short idadeShort = 32767;
+        int age = (int) 10000000000L;
+        long numeroGrande = (long)155.23;
+        double salarioDouble = 2500.0D;
+        float salarioFloat = (float) 2500.0F;
+        byte idadeByte = 127;
+        short idadeShort = 32000;
         boolean verdadeiro = true;
         boolean falso = false;
         char caractere = '\u0041'; /**
@@ -19,5 +19,7 @@ public class Aula02TiposPrimitivos {
         System.out.println("Idade: " + age + " anos.");
         System.out.println(falso);
         System.out.println("CHAR: " + caractere);
+        System.out.println("Age: type cast: " + age);
+        System.out.println("Número grande: " + numeroGrande);
     }
 }
