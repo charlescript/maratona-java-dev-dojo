4965e06ec991ff1e09dfbcc70df48a692b251d8b
charlescript <rocha_charles@outlook.com>
2026-01-04 21:28:55 -0300

    16/286 Operadores aritméticos e type casting

---
+++ maratona-java/src/main/java/academy/devdojo/maratonajava/introducao/Aula04Operadores.java
@@ -0,0 +1,12 @@
+package academy.devdojo.maratonajava.introducao;
+
+public class Aula04Operadores {
+  public static void main(String[] args) {
+    // + - / *
+    int numero01 = 10;
+    int numero02 = 20;
+    double resultado = ((double)numero01 / numero02); //type casting
+
+    System.out.println("Valor: " + resultado );
+  }
+}
