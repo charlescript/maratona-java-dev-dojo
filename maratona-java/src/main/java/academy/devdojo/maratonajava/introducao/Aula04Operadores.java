33f5881436604fd775c6562ab9d7bcdbcc7a9fd8
charlescript <rocha_charles@outlook.com>
2026-01-04 22:01:02 -0300

    17/286 Operadores pt2 -> Booleanos

--- maratona-java/src/main/java/academy/devdojo/maratonajava/introducao/Aula04Operadores.java
+++ maratona-java/src/main/java/academy/devdojo/maratonajava/introducao/Aula04Operadores.java
@@ -3,10 +3,35 @@ package academy.devdojo.maratonajava.introducao;
 public class Aula04Operadores {
   public static void main(String[] args) {
     // + - / *
+    System.out.println("Operadores aritméticos: ");
     int numero01 = 10;
     int numero02 = 20;
     double resultado = ((double)numero01 / numero02); //type casting
+    System.out.println("Valor: " + resultado);
 
-    System.out.println("Valor: " + resultado );
+    System.out.println("__________________________________");
+
+    // Operador de resto -> MOD %
+    System.out.println("Operador módulo captura resto:");
+    int resto = 21 % 7;
+    System.out.println("Módulo: " + resto);
+
+    System.out.println("__________________________________");
+
+    /** < > | <= |  >= |  == | !=
+     * */
+    System.out.println("Operador boleano, true ou false: ");
+    boolean isDezMaiorQueVinte = 10 > 20;
+    boolean isDezMenorQueVinte = 10 < 20;
+    boolean isDezIgualVinte = 10 == 20;
+    boolean isDezIgualDez = 10 == 10;
+    boolean isDezDiferenteDez = 10 != 10;
+    System.out.println("isDezMaiorQueVinte: " + isDezMaiorQueVinte);
+    System.out.println("isDezMenorQueVinte: " + isDezMenorQueVinte);
+    System.out.println("isDezIgualVinte: " + isDezIgualVinte);
+    System.out.println("isDezIgualDez: " + isDezIgualDez);
+    System.out.println("isDezDiferenteDez: " + isDezDiferenteDez);
+
+    System.out.println("__________________________________");
   }
 }
