cb92c4e88575a32b0c05d693df73c40d0546f370
charlescript <rocha_charles@outlook.com>
2026-01-04 23:42:11 -0300

    20/286 Operadores de atribuição

--- maratona-java/src/main/java/academy/devdojo/maratonajava/introducao/Aula04Operadores.java
+++ maratona-java/src/main/java/academy/devdojo/maratonajava/introducao/Aula04Operadores.java
@@ -58,7 +58,25 @@ public class Aula04Operadores {
 
     System.out.println("__________________________________");
 
+    /** Operadores de atribuição
+      =  , += , -= , *= , /= , %= ,
+    */
+    double bonus = 1800; // 1800
+    bonus += 1000;  // 2800
+    bonus -= 1000;  // 1800
+    bonus *= 2;   // 3600
+    bonus /= 2;   // 1800
+    bonus %= 2;  // 00
+    System.out.println("Bonus: " + bonus);
 
-
+    // ++    --
+    int contador = 0;
+    contador += 1;   // contador = contador + 1
+    contador++;
+    contador--;
+    ++contador;
+    --contador;
+    int contador2 = 0;
+    System.out.println(++contador2);
   }
 }
