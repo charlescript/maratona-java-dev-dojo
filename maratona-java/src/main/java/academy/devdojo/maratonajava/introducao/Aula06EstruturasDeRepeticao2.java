20632990865e3d261b3362048cdc6540b2071af7
charlescript <rocha_charles@outlook.com>
2026-01-07 14:54:25 -0300

    29/286 Exercicio repetição e validação

---
+++ maratona-java/src/main/java/academy/devdojo/maratonajava/introducao/Aula06EstruturasDeRepeticao2.java
@@ -0,0 +1,18 @@
+package academy.devdojo.maratonajava.introducao;
+
+public class Aula06EstruturasDeRepeticao2 {
+  public static void main(String[] args) {
+    // Imprima todos os números pares de 0 até 1000000
+
+    for(int i = 0; i <= 1000000; i++){
+      if( i % 2 == 0 ){
+        System.out.println(i);
+      }
+    }
+
+    /** for(int i = 0; i <= 1000000; i+=2){
+      System.out.println(i);
+    } */
+
+  }
+}
