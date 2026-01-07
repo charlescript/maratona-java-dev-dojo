60fc8845395688a2a42070c3e6a59309dafe7e87
charlescript <rocha_charles@outlook.com>
2026-01-07 15:14:31 -0300

    30/286 Estruturas repetição 03 -> Break

--- maratona-java/src/main/java/academy/devdojo/maratonajava/introducao/Aula06EstruturasDeRepeticao02.java
+++ maratona-java/src/main/java/academy/devdojo/maratonajava/introducao/Aula06EstruturasDeRepeticao02.java
@@ -1,6 +1,6 @@
 package academy.devdojo.maratonajava.introducao;
 
-public class Aula06EstruturasDeRepeticao2 {
+public class Aula06EstruturasDeRepeticao02 {
   public static void main(String[] args) {
     // Imprima todos os números pares de 0 até 1000000
 
@@ -10,9 +10,6 @@ public class Aula06EstruturasDeRepeticao2 {
       }
     }
 
-    /** for(int i = 0; i <= 1000000; i+=2){
-      System.out.println(i);
-    } */
 
   }
 }
