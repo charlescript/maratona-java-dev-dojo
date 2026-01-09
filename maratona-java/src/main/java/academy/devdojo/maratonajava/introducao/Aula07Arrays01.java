8d03032f0b2634d3ac9155f7da91a931cdac348c
charlescript <rocha_charles@outlook.com>
2026-01-09 03:40:49 -0300

    34/286 Arrays pt2 tipos

--- maratona-java/src/main/java/academy/devdojo/maratonajava/introducao/Aula07Arrays01.java
+++ maratona-java/src/main/java/academy/devdojo/maratonajava/introducao/Aula07Arrays01.java
@@ -7,7 +7,8 @@ public class Aula07Arrays01 {
             idades[1] = 15;
             idades[2] = 11;
 
-
             System.out.println(idades[0]);
+            System.out.println(idades[1]);
+            System.out.println(idades[2]);
       }
 }
