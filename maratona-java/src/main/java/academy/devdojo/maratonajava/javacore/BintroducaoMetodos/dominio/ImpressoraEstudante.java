1c23e47fa258d7b63e178240982f58dfa180aab1
charlescript <rocha_charles@outlook.com>
2026-01-12 03:35:25 -0300

    50/286 POO Métodos pt->06 - Parâmetros tipo referência, Impressora Estudante

---
+++ maratona-java/src/main/java/academy/devdojo/maratonajava/javacore/BintroducaoMetodos/dominio/ImpressoraEstudante.java
@@ -0,0 +1,13 @@
+package academy.devdojo.maratonajava.javacore.BintroducaoMetodos.dominio;
+
+import academy.devdojo.maratonajava.javacore.BintroducaoMetodos.dominio.ImpressoraEstudante;
+public class ImpressoraEstudante {
+
+  public void imprime(Estudante estudante){
+    System.out.println("-----------PRINTER------------------");
+
+    System.out.println(estudante.nome);
+    System.out.println(estudante.sexo);
+    System.out.println(estudante.idade);
+  }
+}
