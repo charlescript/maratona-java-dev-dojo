96e4e1b8a52cff6f825bdf5e341934cccb7b679b
charlescript <rocha_charles@outlook.com>
2026-01-13 15:21:04 -0300

    POO -> Modificador de acesso private, get e set pt->02

--- maratona-java/src/main/java/academy/devdojo/maratonajava/javacore/BintroducaoMetodos/test/PessoaTest01.java
+++ maratona-java/src/main/java/academy/devdojo/maratonajava/javacore/BintroducaoMetodos/test/PessoaTest01.java
@@ -7,10 +7,7 @@ public class PessoaTest01 {
      Pessoa pessoa = new Pessoa();
      pessoa.setNome("Jiraya");
      pessoa.setIdade(70);
-     //pessoa.imprime();
-     
-     System.out.println(pessoa.getNome());
-     System.out.println(pessoa.getIdade());
+     pessoa.imprime();
 
    }
 }
