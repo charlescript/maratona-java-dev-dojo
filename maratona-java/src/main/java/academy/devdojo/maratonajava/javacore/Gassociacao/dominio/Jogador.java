a0e184811f2a8360a7fb62787a5089abb440663b
charlescript <rocha_charles@outlook.com>
2026-01-16 05:11:26 -0300

    66/286 POO -> Associação pt->02 -> Associação unidirecional um para muitos Time(1)---->Jogador(N) muitos

--- maratona-java/src/main/java/academy/devdojo/maratonajava/javacore/Gassociacao/dominio/Jogador.java
+++ maratona-java/src/main/java/academy/devdojo/maratonajava/javacore/Gassociacao/dominio/Jogador.java
@@ -2,13 +2,34 @@ package academy.devdojo.maratonajava.javacore.Gassociacao.dominio;
 
 public class Jogador {
     private String nome;
-
-    public void imprime() {
-        System.out.println(this.nome);
-    }
+    private String posicao = "A definir";
+    private Time time;
 
     public Jogador(String nome){
         this.nome = nome;
+    }
+
+    public Jogador(String nome, String posicao){
+      this(nome);
+      this.posicao = posicao;
+    }
+
+    public void imprime() {
+        System.out.println("Jogador: " + this.nome);
+        System.out.println("Posição: " + this.posicao);
+        if(time != null) {
+          System.out.println("Time: " + time.getNome());
+        }
+
+       System.out.println("--------------------------------");
+    }
+
+    public Time getTime(){
+      return time;
+    }
+
+    public void setTime(Time time){
+      this.time = time;
     }
 
     public String getNome(){
@@ -17,6 +38,14 @@ public class Jogador {
 
     public void setNome(String nome){
         this.nome = nome;
+    }
+
+    public String getPosicao(){
+      return posicao;
+    }
+
+    public void setPosicao(){
+      this.posicao = posicao;
     }
 
 }
