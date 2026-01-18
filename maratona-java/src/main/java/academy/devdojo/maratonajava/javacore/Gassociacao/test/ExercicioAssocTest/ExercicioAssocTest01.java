c3c80dcf5c0bc9d5799e8aa1ecf727cea8b6c362
charlescript <rocha_charles@outlook.com>
2026-01-18 03:15:47 -0300

    Feach: Criando as Classes Pai e importando dentro da classe ExercicioAssocTest01 para instacias e test do projeto

---
+++ maratona-java/src/main/java/academy/devdojo/maratonajava/javacore/Gassociacao/test/ExercicioAssocTest/ExercicioAssocTest01.java
@@ -0,0 +1,13 @@
+package academy.devdojo.maratonajava.javacore.Gassociacao.test.ExercicioAssocTest;
+
+import academy.devdojo.maratonajava.javacore.Gassociacao.dominio.ExcercicioAssoc.Local;
+import academy.devdojo.maratonajava.javacore.Gassociacao.dominio.ExcercicioAssoc.Seminario;
+import academy.devdojo.maratonajava.javacore.Gassociacao.dominio.ExcercicioAssoc.Professor;
+import academy.devdojo.maratonajava.javacore.Gassociacao.dominio.ExcercicioAssoc.Aluno;
+
+
+public class ExercicioAssocTest01 {
+   public static void main(String[] args){
+
+   }
+}
