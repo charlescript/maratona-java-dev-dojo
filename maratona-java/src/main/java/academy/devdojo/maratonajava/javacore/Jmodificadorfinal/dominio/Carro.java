852417cc16382f68d3e6a7110162682fa9f08190
charlescript <rocha_charles@outlook.com>
2026-01-22 00:04:08 -0300

    Modificador final pt-01 sem static

--- maratona-java/src/main/java/academy/devdojo/maratonajava/javacore/Jmodificadorfinal/dominio/Carro.java
+++ maratona-java/src/main/java/academy/devdojo/maratonajava/javacore/Jmodificadorfinal/dominio/Carro.java
@@ -1,4 +1,20 @@
 package academy.devdojo.maratonajava.javacore.Jmodificadorfinal.dominio;
 
 public class Carro {
+   private String nome;
+   public final double VELOCIDADE_LIMITE;
+
+   {
+     VELOCIDADE_LIMITE = 250;
+   }
+
+   public String getNome(){
+     return nome;
+   }
+
+   public void setNome(String nome){
+     this.nome = nome;
+   }
+
+
 }
