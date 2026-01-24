ca41bfc1c8b0690beaf9f1335879b6676f02e73d
charlescript <rocha_charles@outlook.com>
2026-01-24 10:02:16 -0300

    82/286 POO -> Enumeração pt->02 - Construtores atributos

--- maratona-java/src/main/java/academy/devdojo/maratonajava/javacore/Kenum/test/ClienteTest01.java
+++ maratona-java/src/main/java/academy/devdojo/maratonajava/javacore/Kenum/test/ClienteTest01.java
@@ -5,17 +5,13 @@ import academy.devdojo.maratonajava.javacore.Kenum.dominio.TipoCliente;
 
 public class ClienteTest01 {
   public static void main(String[] args){
-     Cliente cliente1 = new Cliente("Tenchihan", TipoCliente.PESSOA_FISICA);
-     Cliente cliente2 = new Cliente("Goku", TipoCliente.PESSOA_JURIDICA);
-     Cliente cliente3 = new Cliente("Vegeta", TipoCliente.PESSOA_FISICA);
-     Cliente cliente4 = new Cliente("Freeza", TipoCliente.PESSOA_JURIDICA);
-     Cliente cliente5 = new Cliente("Family Tech", TipoCliente.HOLDING_FAMILIAR);
+     Cliente cliente1 = new Cliente("Marieta", TipoCliente.PESSOA_FISICA, Cliente.TipoPagamento.DEBITO);
+     Cliente cliente2 = new Cliente("Libra", TipoCliente.PESSOA_JURIDICA, Cliente.TipoPagamento.PIX);
+     Cliente cliente3 = new Cliente("BTP", TipoCliente.HOLDING_FAMILIAR, Cliente.TipoPagamento.CREDITO);
 
      System.out.println(cliente1);
      System.out.println(cliente2);
      System.out.println(cliente3);
-     System.out.println(cliente4);
-     System.out.println(cliente5);
 
   }
 }
