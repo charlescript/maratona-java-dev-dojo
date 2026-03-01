4f8b9a116d3d096f5285180011ea86630274821c
charlescript <rocha_charles@outlook.com>
2026-03-01 02:17:12 -0300

    105/286 104 - Exceções pt 10 - Exceção customizada

--- maratona-java/src/main/java/academy/devdojo/maratonajava/javacore/Oexception/exception/test/LoginInvalidoExceptionTest01.java
+++ maratona-java/src/main/java/academy/devdojo/maratonajava/javacore/Oexception/exception/test/LoginInvalidoExceptionTest01.java
@@ -1,4 +1,36 @@
 package academy.devdojo.maratonajava.javacore.Oexception.exception.test;
 
+import academy.devdojo.maratonajava.javacore.Oexception.exception.dominio.LoginInvalidoException;
+
+import java.util.Scanner;
+
 public class LoginInvalidoExceptionTest01 {
+
+   public static void main(String[] args) {
+
+      try {
+        logar();
+      } catch(LoginInvalidoException e) {
+        e.printStackTrace();
+      }
+   }
+
+   private static void logar() throws LoginInvalidoException {
+     Scanner teclado = new Scanner(System.in);
+     String usernameDb = "Goku";
+     String senhaDb = "ssj";
+
+     System.out.println("Usuário: ");
+     String usernameDigitado = teclado.next();
+
+     System.out.println("Senha: ");
+     String senhaDigitada = teclado.next();
+
+     if( !usernameDb.equals(usernameDigitado) || !senhaDb.equals(senhaDigitada) ){
+        throw new LoginInvalidoException("Usuário ou senha inválidos");
+     }
+
+     System.out.println("Usuário logado com sucesso");
+
+   }
 }
